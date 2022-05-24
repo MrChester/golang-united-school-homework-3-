1fc46498df0a166f0a9203d20980e378d97d1bea
Yauheni Ramankou <iromankov2010@gmail.com>
2022-05-24 16:29:52 +0300

    feat: implement reverse task

--- task02-slice.go
+++ task02-slice.go
@@ -1,5 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
+	var lastNdx int = len(input) - 1
+	for i := lastNdx; i >= 0; i-- {
+		result = append(result, input[i])
+	}
 	return
 }
