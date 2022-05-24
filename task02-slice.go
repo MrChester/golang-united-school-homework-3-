832e32a514d62a7762c43f6f3907047327531812
Yauheni Ramankou <iromankov2010@gmail.com>
2022-05-24 18:36:48 +0300

    refactor: add  code formatting to reverse task

--- task02-slice.go
+++ task02-slice.go
@@ -2,8 +2,10 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	var lastNdx int = len(input) - 1
+
 	for i := lastNdx; i >= 0; i-- {
 		result = append(result, input[i])
 	}
+
 	return
 }
