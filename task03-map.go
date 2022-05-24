a026ea48d601378516e08ccd9ad4c5fc5afbd38e
Yauheni Ramankou <iromankov2010@gmail.com>
2022-05-24 18:28:38 +0300

    feat: implement map sort task

--- task03-map.go
+++ task03-map.go
@@ -1,5 +1,19 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
+	var keys []int
+
+	for key := range input {
+		keys = append(keys, key)
+	}
+
+	sort.Ints(keys)
+
+	for _, elem := range keys {
+		result = append(result, input[elem])
+	}
+
 	return
 }
