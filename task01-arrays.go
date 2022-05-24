59bd492e61000d460ffd3e997a5f1439bd62ba36
Yauheni Ramankou <iromankov2010@gmail.com>
2022-05-24 16:03:43 +0300

    feat: implement average task

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,5 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	return
+	var sum float32
+
+	for _, val := range input {
+		sum += val
+	}
+	return sum / float32(len(input))
 }
